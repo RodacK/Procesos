cd88bcec2b34352c93c18ed1592633d8255c2946
johnn <johnn@jnetg-PC>
2017-09-03 15:45:13 -0500

    ..

--- Operativos/src/app/Principal.java
+++ Operativos/src/app/Principal.java
@@ -14,36 +14,35 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
-	 private Stage primaryStage;
-	 private Pane  panel;
+	private Stage primaryStage;
+	private Pane panel;
+
 	@Override
 	public void start(Stage primaryStage) {
 
-		 this.primaryStage = primaryStage;
-	     this.primaryStage.setTitle("Procesos");
-	     initLayout();
-	     primaryStage.setFullScreen(true);
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Procesos");
+		initLayout();
+		primaryStage.setFullScreen(true);
 	}
+
 	public void initLayout() {
-        try {
+		try {
 
-        	FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Principal.class.getResource("view/Principal.fxml"));
-            panel = (Pane) loader.load();
-            PrincipalController controller = loader.getController();
-            controller.setPrincipal(this);
-            Scene scene = new Scene(panel);
-            primaryStage.setScene(scene);
-            primaryStage.show();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Principal.class.getResource("view/Principal.fxml"));
+			panel = (Pane) loader.load();
+			PrincipalController controller = loader.getController();
+			controller.setPrincipal(this);
+			Scene scene = new Scene(panel);
+			primaryStage.setScene(scene);
+			primaryStage.show();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-	public Principal(){
-
-	 }
 	public static void main(String[] args) {
 		launch(args);
 	}
